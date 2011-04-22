3cf607e4af69660c7f4ebd3b0bdacb8b5efded54
Schnittchen <schnittchen@das-labor.org>
2011-04-22 22:59:16 +0200

    ported has_scalars.rb

--- lib/libbracket/has_scalars.rb
+++ lib/libbracket/has_scalars.rb
@@ -1,14 +1,12 @@
 require 'libbracket/is_summable'
 
 module LibBracket
-  class ScalarMultiple < CompositeTerm
-    def initialize(scalar, other)
-      h = { :scalar => scalar, :other => other }
-      super other.domain, ChildrenHash.new.replace(h)
-    end
+  module ScalarMultiple
+    include PrimitiveWithChildren
     
     def self.from_scalar_and_other(scalar, other)
-      new scalar, other
+      h = { :scalar => scalar, :other => other }
+      return Term.construct ScalarMultiple, other.domain, ChildrenHash.new.replace(h)
     end
     
     def scalar
@@ -19,36 +17,30 @@ module LibBracket
       @children[:other]
     end
     
-    STATE_SCALAR_DISTRIBUTED = :state_scalar_distributed
-    STATE_OTHER_DISTRIBUTED = :state_other_distributed
-    STATE_MULTIPLES_MERGED = :state_multiples_merged
-    STATE_ONE_AND_ZERO_CHECKED = :state_one_and_zero_checked
-    
-    canonicalize_children_first
-    next_cstep :cdistribute_scalar, STATE_SCALAR_DISTRIBUTED
-    next_cstep :cdistribute_other, STATE_OTHER_DISTRIBUTED
-    next_cstep :cmerge_multiples, STATE_MULTIPLES_MERGED
-    next_cstep :ccheck_one_and_zero, STATE_ONE_AND_ZERO_CHECKED
-    canonical_at STATE_ONE_AND_ZERO_CHECKED
+    CFRAGMENT = CanonicalizationFragment.new
+    CFRAGMENT.declare_step :cdistribute_scalar
+    CFRAGMENT.declare_step :cdistribute_other
+    CFRAGMENT.declare_step :cmerge_multiples
+    CFRAGMENT.declare_step :ccheck_one_and_zero
     
     def cdistribute_scalar
       s, o = scalar, other
       return nil unless s.is_a? Sum
-      summands = s.children.collect { |term| ScalarMultiple.new term, o }
-      return Sum.new *summands
+      summands = s.children.collect { |term| ScalarMultiple.from_scalar_and_other term, o }
+      return Sum.from_summands *summands
     end
     
     def cdistribute_other
       s, o = scalar, other
       return nil unless o.is_a? Sum
-      summands = o.children.collect { |term| ScalarMultiple.new s, term }
-      return Sum.new *summands
+      summands = o.children.collect { |term| ScalarMultiple.from_scalar_and_other s, term }
+      return Sum.from_summands *summands
     end
     
     def cmerge_multiples
       s, o = scalar, other
       return nil unless o.is_a? ScalarMultiple
-
+      
       if @domain.include? HasScalarsFromRight
         return ScalarMultiple.from_scalar_and_other(o.scalar * s, o.other)
       else
@@ -65,18 +57,17 @@ module LibBracket
     end
     
     def render(rctxt)
-      str = if @domain.include? HasScalarsFromRight
-        "#{other.render MUL}*#{scalar.render MUL}"
-      else
-        "#{scalar.render MUL}*#{other.render MUL}"
-      end
-      return OperatorBinding.bracket_if_needed str, rctxt, MUL
+      inner = [other.render MUL, scalar.render MUL]
+      inner.reverse! if @domain.include? HasScalarsFromRight
+      return OperatorBinding.bracket_if_needed inner.join("*"), rctxt, MUL
     end
   end
-  
+    
   module HasScalars
+    
+    
     def scalar_multiple(scalar)
-      return ScalarMultiple.new scalar, self
+      return ScalarMultiple.from_scalar_and_other scalar, self
     end
   end
   
@@ -90,7 +81,7 @@ module LibBracket
   
   module ScalarMultipleOperator
     def *(scalar)
-      return ScalarMultiple.new scalar, self
+      return ScalarMultiple.from_scalar_and_other scalar, self
     end
   end
 end
