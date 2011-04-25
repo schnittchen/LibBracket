cbb9acbbc367f8a341eea81f5a1d60b3c7e1a80d
Schnittchen <schnittchen@das-labor.org>
2011-04-25 13:05:03 +0200

    init order

--- lib/libbracket.rb
+++ lib/libbracket.rb
@@ -3,11 +3,13 @@ require 'libbracket/helpers'
 require 'libbracket/chash'
 require 'libbracket/knowledgebase'
 
+
 require 'libbracket/term'
+
+#this should eventually be in common.rb
+require 'libbracket/autoload'
+
 
 #these depend on term:
 require 'libbracket/compositeterm'
 require 'libbracket/atom'
-
-#autoload for all other features
-require 'libbracket/autoload'
