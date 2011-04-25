9b58cf51f7fd545b6435318e705ada2ea2fb6098
Schnittchen <schnittchen@das-labor.org>
2011-04-25 13:16:43 +0200

    renamed helpers.rb to common.rb

--- lib/libbracket.rb
+++ lib/libbracket.rb
@@ -1,14 +1,10 @@
 #needed by term
-require 'libbracket/helpers'
+require 'libbracket/common'
 require 'libbracket/chash'
 require 'libbracket/knowledgebase'
 
 
 require 'libbracket/term'
-
-#this should eventually be in common.rb
-require 'libbracket/autoload'
-
 
 #these depend on term:
 require 'libbracket/compositeterm'
